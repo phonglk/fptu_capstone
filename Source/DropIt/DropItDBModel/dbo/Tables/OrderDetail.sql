45181c9107a877b3c9fd9e119013b7e0b40772c2
Khoi-Phong Le <phonglk@users.noreply.github.com>
2013-06-02 20:07:07 +0000

    Delete Order

--- Source/DropIt/DropItDBModel/dbo/Tables/OrderDetail.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[OrderDetail] (
-    [OrderDetailId] INT IDENTITY (1, 1) NOT NULL,
-    [OrderId]       INT NOT NULL,
-    [TicketId]      INT NOT NULL,
-    CONSTRAINT [PK_OrderDetails] PRIMARY KEY CLUSTERED ([OrderDetailId] ASC),
-    CONSTRAINT [FK_OrderDetail_Order] FOREIGN KEY ([OrderId]) REFERENCES [dbo].[Order] ([OrderId]) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT [FK_OrderDetail_Ticket] FOREIGN KEY ([TicketId]) REFERENCES [dbo].[Ticket] ([TicketId])
-);
-
