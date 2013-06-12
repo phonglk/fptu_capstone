2da25587bec0939bf8721fffcb784a2f95120bc8
laputa <laputa@985307da-06f8-48a4-afca-56d4b63e8d39>
2013-06-12 11:19:40 +0000

    tungdn xóa bỏ try catch

--- Source/DropIt/DropIt/Controllers/UserController.cs
+++ Source/DropIt/DropIt/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using DropIt.Filters;
 using DropIt.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
