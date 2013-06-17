f10ab265ac5195e1ed13442f731d242ef36bd74a
laputa <laputa@985307da-06f8-48a4-afca-56d4b63e8d39>
2013-06-17 03:43:02 +0000

    thêm vào TicketRepository ticketRepository

--- trunk/Source/DropIt/DropIt/DAL/UnitOfWork.cs
+++ trunk/Source/DropIt/DropIt/DAL/UnitOfWork.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using DropIt.Models;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 
 namespace DropIt.DAL
 {
@@ -14,6 +16,7 @@ namespace DropIt.DAL
         private GenericRepository<Venue> venueRepository;
         private GenericRepository<Province> provinceRepository;
         private UserRepository userRepository;
+        private TicketRepository ticketRepository; 
 
 
         public EventRepository EventRepository
@@ -81,9 +84,39 @@ namespace DropIt.DAL
             }
         }
 
+        public TicketRepository TicketRepository
+        {
+            get
+            {
+
+                if (this.ticketRepository == null)
+                {
+                    this.ticketRepository = new TicketRepository(context);
+                }
+                return ticketRepository;
+            }
+        }
+
         public void Save()
         {
-            context.SaveChanges();
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Debug.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Debug.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
         }
 
         private bool disposed = false;
