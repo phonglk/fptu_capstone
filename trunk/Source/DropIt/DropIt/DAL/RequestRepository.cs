5c4e9f3e8776c9362c35078f8248a400c2df73de
laputa <laputa@985307da-06f8-48a4-afca-56d4b63e8d39>
2013-06-18 08:03:45 +0000

    tungdn thêm RequestRepository

---
+++ trunk/Source/DropIt/DropIt/DAL/RequestRepository.cs
@@ -0,0 +1,17 @@
+﻿using DropIt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DropIt.DAL
+{
+    public class RequestRepository : GenericRepository<Request>
+    {
+        public RequestRepository(DropItContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
