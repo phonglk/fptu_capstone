3ea2fc03fc47863cdf40ab4d174d8eb57ef29be7
laputa <laputa@985307da-06f8-48a4-afca-56d4b63e8d39>
2013-06-20 04:54:52 +0000

    tungdn RequestTicket.cs model đăng vé

---
+++ trunk/Source/DropIt/DropIt/ViewModels/RequestTicket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DropIt.ViewModels
+{
+    public class RequestTicket
+    {
+        public RequestTicket()
+        {
+            this.CreatedDate = DateTime.Now;
+            this.ModifiedDate = DateTime.Now;
+        }
+
+        public int UserId { get; set; }
+
+        public int? EventId { get; set; }
+
+        public int Status { get; set; }
+
+        public string Description { get; set; }
+
+        public string EventName { get; set; }
+
+        public string VenueName { get; set; }
+
+        public string Address { get; set; }
+
+        public int? ProvinceId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int? VenueId { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? HoldDate { get; set; }
+
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+    }
+}
