2a5a7c90a4b0ff4aa032523881f142ba0b8acbbc
quocduydang3000 <quocduydang3000@985307da-06f8-48a4-afca-56d4b63e8d39>
2013-06-25 07:24:41 +0000

    request

---
+++ trunk/Source/DropIt/DropIt/Areas/Administration/Controllers/RequestController.cs
@@ -0,0 +1,119 @@
+﻿using DropIt.Common;
+using DropIt.DAL;
+using DropIt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropIt.Areas.Administration.Controllers
+{
+    public class RequestController : Controller
+    {
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
+        private RequestRepository Repository;
+
+        public RequestController()
+        {
+            Repository = unitOfWork.RequestRepository;
+        }
+        //
+        // GET: /Administration/Request/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult List(int jtStartIndex = -1, int jtPageSize = 0, string jtSorting = null)
+        {
+            try
+            {
+
+                var records = Repository.JTGet(jtStartIndex, jtPageSize, jtSorting);
+
+                var Records = records.Select(e => new
+                {
+                    UserId = e.UserId,
+                    EventId = e.EventId,
+                    Status = e.Status,
+                    Description = e.Description,
+                    CreatedDate = e.CreatedDate,
+                    ModifiedDate = e.ModifiedDate
+                });
+                return Json(new JSONResult(Records)
+                {
+                    TotalRecordCount = Repository.Count
+                });
+            }
+            catch (Exception e)
+            {
+                return Json(new JSONResult(e));
+            }
+        }
+        
+        [HttpPost]
+        public JsonResult Create(Request request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return Json(new JSONResult("Form is not valid"));
+                }
+
+                var addedRecord = Repository.AddOrUpdate(request);
+                unitOfWork.Save();
+                return Json(new JSONResult(addedRecord, "Record"));
+
+            }
+            catch (Exception e)
+            {
+                return Json(new JSONResult(e));
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Update(Request request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return Json(new JSONResult("Form is invalid"));
+                }
+
+                Repository.AddOrUpdate(request);
+                unitOfWork.Save();
+                return Json(new JSONResult());
+            }
+            catch (Exception e)
+            {
+                return Json(new JSONResult(e));
+            }
+        }
+
+        //public JsonResult Delete(int UserId )
+        //{
+        //    try
+        //    {
+        //        Repository.Delete(VenueId);
+        //        unitOfWork.Save();
+
+        //        return Json(new JSONResult());
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return Json(new JSONResult(e));
+        //    }
+
+        //}
+
+
+
+    }
+}
