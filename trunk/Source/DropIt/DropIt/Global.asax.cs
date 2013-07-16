9e0677eb1d0bea0eaec079312a1125db498003dd
phonglk <phonglk@985307da-06f8-48a4-afca-56d4b63e8d39>
2013-07-16 06:04:11 +0000

    Update Admin Event

--- trunk/Source/DropIt/DropIt/Global.asax.cs
+++ trunk/Source/DropIt/DropIt/Global.asax.cs
@@ -3,10 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using DropIt.DAL;
+using DropIt.Common;
+using System.Diagnostics;
 
 namespace DropIt
 {
@@ -15,6 +19,9 @@ namespace DropIt
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static CacheItemRemovedCallback OnCacheRemove = null;
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -29,6 +36,33 @@ namespace DropIt
             ModelBinders.Binders.Add(typeof(DateTime?), new NullableDateTimeBinder());
 
             DropIt.Common.AdminMenu.Init();
+
+            AddTask("CheckEventExpire", 10);
+        }
+
+        private void AddTask(string name, int seconds)
+        {
+            OnCacheRemove = new CacheItemRemovedCallback(CacheItemRemoved);
+            HttpRuntime.Cache.Insert(name, seconds, null,
+                DateTime.Now.AddSeconds(seconds), Cache.NoSlidingExpiration,
+                CacheItemPriority.NotRemovable, OnCacheRemove);
+        }
+
+        public void CacheItemRemoved(string k, object v, CacheItemRemovedReason r)
+        {
+            if(k.Equals("CheckEventExpire")){
+
+
+                //var EventsNeedExpire = unitOfWork.EventRepository.Get(e => e.Status != (int)Statuses.Event.Outdate && e.HoldDate <= DateTime.Now);
+                //foreach (Event Event in EventsNeedExpire)
+                //{
+                //    Event.Status = (int)Statuses.Event.Outdate;
+                //    unitOfWork.EventRepository.AddOrUpdate(Event);
+                //}
+                //unitOfWork.Save();
+                
+            }
+            AddTask(k, Convert.ToInt32(v));
         }
     }
 }
